b1632e43bf1e1395858df77cabb1f3ba9c72405e
FLX007 <aamiran3007@yandex.ru>
2021-11-28 16:33:22 +0300

    16:33

--- CurrencyConverter/Models/Exchange.swift
+++ CurrencyConverter/Models/Exchange.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+struct Exchange: Decodable {
+    let base: String
+    let rates: ExchangeRates
+}
+
+struct ExchangeRates: Decodable {
+    let EUR: Double
+    let RUB: Double
+    let GBP: Double
+    let CNY: Double
+}
