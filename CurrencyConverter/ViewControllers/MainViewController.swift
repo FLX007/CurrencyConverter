4642080b21fefea6dc074db03d0b19bf56f0f6d3
FLX007 <aamiran3007@yandex.ru>
2021-11-28 16:20:26 +0300

    16:20

--- CurrencyConverter/ViewControllers/MainViewController.swift
+++ CurrencyConverter/ViewControllers/MainViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    @IBOutlet weak var sourceAmountTextField: UITextField!
+    @IBOutlet weak var destAmountTextField: UITextField!
+    @IBOutlet weak var destAmountLabel: UILabel!
+    
+    private var currencies: [String] = []
+    private var exchange: Exchange!
+    private var lastDirection: Direction = .srcToDest
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
